b97e0bc521974f7f9b944c44e15a878a6a9103d3
Damola Adediran <tylher123@gmail.com>
2022-06-20 15:25:43 +0100

    Create student class

--- person.rb
+++ person.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Person
   def initialize(age, id, name = 'unknown', parent_permission: true)
     @id = id
--- student.rb
+++ student.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+require './person'
 
 class Student < Person
   def initialize(classroom)
