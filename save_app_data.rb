2694481d3e07eb3d7f8e3c94244ffa46d67aabf8
Fsher07 <furkansalimm@gmail.com>
2022-06-29 18:58:08 +0300

    Add a function to save people

--- save_app_data.rb
+++ save_app_data.rb
@@ -4,8 +4,9 @@ require './rental'
 require 'json'
 
 module SaveData
-  def save_data(books)
+  def save_data(books, people)
     save_books(books)
+    save_person(people)
   end
 
   def save_books(books)
@@ -18,5 +19,27 @@ module SaveData
     end
     books_data = JSON.generate(saved_books)
     File.write('./data/books.json', books_data)
+  end
+
+  def save_person(people)
+    saved_people = []
+    people.each do |person|
+      if person.instance_of? Student
+        saved_people << {
+          class: 'Student', id: person.id,
+          name: person.name,
+          age: person.age,
+          permission: person.parent_permission
+        }
+      elsif person.instance_of? Teacher
+        saved_people << {
+          class: 'Teacher', id: person.id,
+          name: person.name,
+          age: person.age,
+          specialization: person.specialization
+        }
+      end
+    end
+    File.write('./data/people.json', JSON.generate(saved_people))
   end
 end
