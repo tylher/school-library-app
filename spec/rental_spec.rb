58cefcc567e4b3584715344d25b1f6e1974a012b
Damola Adediran <tylher123@gmail.com>
2022-06-30 14:01:04 +0100

    Fix linters

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -1,15 +1,15 @@
 describe 'Rental class' do
-  it 'returns the correct title of rented book' do
+  before(:all) do
     @person = Person.new(28, 'Ade')
     @book = Book.new('LOTR', 'Tolkien')
-    new_rental = Rental.new('2020', @book, @person)
+    Rental.new('2020', @book, @person)
+  end
+
+  it 'returns the correct title of rented book' do
     expect(@book.rentals[0].book.title).to eq('LOTR')
   end
 
   it 'returns correct date of rental' do
-    @person = Person.new(28, 'Ade')
-    @book = Book.new('LOTR', 'Tolkien')
-    new_rental = Rental.new('2020', @book, @person)
     expect(@book.rentals[0].date).to eq('2020')
     expect(@person.rentals[0].date).to eq('2020')
   end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 describe Student do
   before(:all) do
-    @student = Student.new('A',23, 'ade')
+    @student = Student.new('A', 23, 'ade')
   end
   it 'Check if object is of class Person' do
     expect(@student).to_not be_instance_of Person
@@ -9,13 +9,11 @@ describe Student do
   it 'Check if object is of class Student' do
     expect(@student).to be_instance_of Student
   end
-  it "capitalize name" do
+  it 'capitalize name' do
     student_update = CapitalizeDecorator.new(@student)
     expect(student_update.correct_name).to eql('Ade')
   end
-  it "confirm classroom" do
+  it 'confirm classroom' do
     expect(@student.classroom).to eq('A')
   end
-  
-  
 end
