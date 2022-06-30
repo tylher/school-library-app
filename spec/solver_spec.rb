e9b52789d6d0da2d4db4331ade972faefe414a5f
Fsher07 <furkansalimm@gmail.com>
2022-06-30 18:34:51 +0300

    Add unit test for negative number exception

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -6,4 +6,7 @@ describe Solver do
   it 'calls the factorial method to return the factorial of given input' do
     expect(solver.factorial(5)).to eq 120
   end
+
+  it 'raise an exception when a negative integer is entered' do
+    expect(solver.factorial(-2).to raise_error(StandarError))
 end
